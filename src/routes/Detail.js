21f79f218a62d8974ec5ac6ac0437fcc48f7fd3d
nameless0422 <cjy042218@gmail.com>
2022-01-21 16:07:11 +0900

    movie app done

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,31 +1,16 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Movie from "../components/Movie";
-
-function Detail(){
-    const {id} = useParams();
-    const [detail , setDetail] = useState();
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const getDetail = async () => {
-        const json = await (
-        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-        ).json();
-        setDetail(json.data.movie);
-        setLoading(false);
-        };
-        getDetail();
-        }, [id]);
-    console.log(detail);
-    return <h1>
-        {loading ? <h1>Loading...</h1> : <Movie 
-            coverImg={detail.medium_cover_image}
-            id={detail.id}
-            title={detail.title}
-            genres={detail.genres}
-            key={detail.id}
-        />}
-    </h1>
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return <h1>Detail</h1>;
 }
-
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home(){
     const [loading, setLoading] = useState(true);
@@ -17,19 +18,24 @@ function Home(){
     },[]);
     console.log(movies)
     return (
-    <div>
-    {loading ? <h1>Loading...</h1> : movies.map(movie => 
-        <Movie 
-            coverImg={movie.medium_cover_image}
-            id={movie.id}
-            title={movie.title}
-            summary={movie.summary}
-            genres={movie.genres}
-            key={movie.id}
-        />
-    )}
-    </div>
-    );
-}
-
+    <div className={styles.container}>
+    {loading ? (<div className={styles.loader}> 
+        <span>Loading...</span>
+        </div>) : (<div className={styles.movies}>
+            {movies.map(movie => 
+                <Movie 
+                coverImg={movie.medium_cover_image}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+                key={movie.id}
+                />
+                )}
+              </div>
+            )}
+          </div>
+        );
+      }
 export default Home;
